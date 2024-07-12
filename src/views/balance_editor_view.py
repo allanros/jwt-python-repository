9d3fc24da6e9201e58a3a3de7814315e6fe084b0
Allan <allan.rosendo@hotmail.com>
2024-07-12 13:16:19 -0300

    adding middlewares to balance route

--- src/views/balance_editor_view.py
+++ src/views/balance_editor_view.py
@@ -10,15 +10,21 @@ class BalanceEditorView(ViewInterface):
     def handle(self, request: HttpRequest) -> HttpResponse:
         new_balance = request.body.get('new_balance')
         user_id = request.params.get('user_id')
+        header_user_id = request.headers.get('uid')
 
-        self.__validate_inputs(user_id=user_id, new_balance=new_balance)
+        self.__validate_inputs(
+            user_id=user_id,
+            new_balance=new_balance,
+            header_user_id=header_user_id
+        )
 
         response = self.__controller.edit(user_id=user_id, new_balance=new_balance)
         return HttpResponse(body={'data': response}, status_code=200)
 
-    def __validate_inputs(self, user_id: any, new_balance: any) -> None:
+    def __validate_inputs(self, user_id: any, new_balance: any, header_user_id: any) -> None:
         if(
             not new_balance
             or not user_id
             or not isinstance(new_balance, float)
+            or int(header_user_id) != int(user_id)
         ): raise Exception('Invalid input')
